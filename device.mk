bf6d6bd7475847b959b8a074662edb76bb940a44
Luofan Chen <clfbbn@gmail.com>
2023-04-19 23:30:45 +0800

    miatoll: Build hostapd libraries
    Since wpa-suppliant is now switched to AIDL, it no longer
    builds the HIDL interfaces that the prebuilt software binaries
    need.
    
    Build the HIDL interface to make the prebuilt executable happy.
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- device.mk
+++ device.mk
@@ -354,6 +354,7 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
+    android.hardware.wifi.hostapd@1.0.vendor \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
