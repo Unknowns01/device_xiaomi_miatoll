c32e99a625a17eba6e3b20d7b4b8a677aa14ed67
Bruno Martins <bgcngm@gmail.com>
2025-04-30 00:24:23 +0100

    miatoll: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,7 +58,6 @@ DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_miatoll
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_miatoll
 
 # Jemalloc
