d0d531f32897be961f29ee307c5ee25e826c2234
JARVIS <41569276+JARVIS-4356@users.noreply.github.com>
2025-04-30 16:25:43 +0000

    sm6250-common: Add support for Sony Dolby Atmos

--- atoll.mk
+++ atoll.mk
@@ -16,6 +16,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Dolby Atmos
+$(call inherit-product, vendor/sony/dolby/dolby.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
