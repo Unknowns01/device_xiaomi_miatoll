b577d6adf01c958a18b774f4edd261bf081e0554
iHSAN <ihsanulrahman@gmail.com>
2025-07-21 23:53:33 +0500

    Revert "miatoll: Switch to NCM USB tethering"
    This reverts commit 7aee427579737fb80568a3c411eac2dde31ae0ab.

--- device.mk
+++ device.mk
@@ -176,7 +176,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
-    NcmTetheringOverlay \
     SM6250DeviceAsWebcam \
     SM6250Frameworks \
     SM6250Settings \
