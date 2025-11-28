ccf0d8bfacd0ffd3748c0d5f751684905874fc2e
minaripenguin <minaripenguin@users.noreply.github.com>
2025-11-29 01:13:06 +0530

    miatoll: props: Disable memtag for various processes
    * it defaults to async if not specified
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -235,6 +235,9 @@ PRODUCT_COPY_FILES += \
 
 # Memory tagging
 PRODUCT_SYSTEM_PROPERTIES += \
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
     persist.arm64.memtag.system_server=off
 
 # NFC
